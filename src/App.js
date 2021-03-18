bc7ca286dcdd49ec18f5ad20107aea8658c7d26d
yejik <yeji9253@gmail.com>
2021-03-18 23:18:26 +0900

    학습: 자신을 호출한 컴포넌트의 state값을 바꾸기

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ class App extends Component {
   // 2. state/props 값이 변경될 때마다 render() 함수가 호출 된다
   // 3. 컴포넌트 정의 시, 첫문자는 항상 대문자로 해야한다 ( ex. <subject> -> <Subject> )
   // 4. react는 유사 자바스크립트 언어로, JS가 아닌 JSX이다
+  // github authorization token:
+  // vscode://vscode.github-authentication/did-authenticate?windowid=3&code=f959d16f6c4bd7380b01&state=9386f14a-2247-4746-b8c7-13b28a19aa21
   constructor(props) {
     super(props);
     this.state = {
       mode: 'read',
+      selectedContentId: 2,
       subject: {title: 'WEB', sub: 'World Wide Web!'},
       welcome: {title: 'welcome', desc:'Hello, React!!'},
       contents:[
@@ -30,16 +33,40 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selectedContentId) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++;
+      }
     }
+
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
         </Subject>
-        <Nav data={this.state.contents}></Nav>
+
+        <Nav 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selectedContentId: Number(id)
+            });
+          }.bind(this)} 
+          data={this.state.contents}
+        >  
+        </Nav>
+
         <Article title={_title} desc={_desc}></Article>
       </div>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,30 @@ class Nav extends Component {
         var data = this.props.data;
         var i=0;
         while(i < data.length) {
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        onClick={function(id, e){
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
+                    >{data[i].title}</a>
+                </li>
+                
+                // 아래 방식으로도 가능함
+                // <li key={data[i].id}>
+                //     <a 
+                //         href={"/content/"+data[i].id}
+                //         data-id={data[i].id} 
+                //         onClick={function(e){
+                //             e.preventDefault();
+                //             var id = e.target.dataset.id;
+                //             this.props.onChangePage(id);
+                //         }.bind(this)}
+                //     >{data[i].title}</a>
+                // </li>
+                )
             i++;
         }
         return (
